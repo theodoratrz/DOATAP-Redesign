869a074ce9f7524e2a0ea683e61ebe407f73aa68
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-01-02 15:43:31 +0200

    sidebar styling done

--- components/sidebar.php
+++ components/sidebar.php
@@ -6,10 +6,13 @@
 
 <link rel="stylesheet" href="css/sidebar.css">
 
-<div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
-	<ul class="list-unstyled ps-0">
+<div class="flex-shrink-0 p-3 bg-white" style="width: 15rem;">
+	<ul class="list-group ps-0">
 		<button class="btn-toggle align-items-center collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
 			Home
+			<div class="sidebar-active-arrow">
+				<i class="fas fa-chevron-right fa-xs"></i>
+			</div>
 		</button>
 		<div class="collapse show" id="home-collapse">
 			<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
@@ -21,10 +24,13 @@
 		</li>
 		<button class="btn-toggle align-items-center collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
 			Dashboard
+			<div class="sidebar-active-arrow">
+				<i class="fas fa-chevron-right fa-xs"></i>
+			</div>
 		</button>
 		<div class="collapse" id="dashboard-collapse">
 			<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-				<li><a href="#" class="link-dark">Overview</a></li>
+				<li><a href="#" class="link-dark">Overviewsaassasoaask</a></li>
 				<li><a href="#" class="link-dark">Weekly</a></li>
 				<li><a href="#" class="link-dark">Monthly</a></li>
 				<li><a href="#" class="link-dark">Annually</a></li>
