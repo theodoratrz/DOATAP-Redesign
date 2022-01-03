9f886a89d39af97dd59e15815db8ebc4dfc387d6
theodoratrz <theodora.trz@gmail.com>
2022-01-03 18:11:13 +0200

    login form

--- public/login.php
+++ public/login.php
@@ -1,18 +1,33 @@
 <link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/navbar.css">
+<link rel="stylesheet" href="css/forms.css">
 
 <?php require_once("../components/template.php"); ?>
 
 <body>
 
 <div class="page-container fluid-container">
-<?php require_once("../components/navbar.php"); ?>
+    <?php require_once("../components/navbar.php"); ?>
 
-</div>
-<div class="page-container fluid-container">
-        <div class="parallax"></div>
-        hello
+    <nav class="below-navbar navbar-expand-lg bg-primary navbar-dark">
+    <div class="container d-flex">
+        Σύνδεση
+        </div>
+    </nav>
+    <div class="login-container-wrapper">
+        <div class="login-container">
+                <div class="mb-3">
+                    <i class="fas fa-user-circle"></i>
+                    <label for="formGroupExampleInput" class="form-label">Όνομα Χρήστη</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput">
+                </div>
+                <div class="mb-3">
+                    <i class="fas fa-key"></i>
+                    <label for="formGroupExampleInput2" class="form-label">Κωδικός</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2">
+                </div>
+        </div>
     </div>
-
-
+</div>
     <?php require_once("../components/footer.php"); ?>
 </body>
