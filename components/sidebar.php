e39df541ca9131b8c75e5f315a359f6763321f5e
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-01-03 02:43:22 +0200

    bug fix

--- components/sidebar.php
+++ components/sidebar.php
@@ -8,26 +8,32 @@
 
 <?php
 
+const pageNames = array(
+	"profile" => array(
+		"" => "Οι πληροφορίες μου",
+		"account" => array(
+			"Ο Λογαριασμός μου",
+			array(
+				"details1" => "1",
+				"details2" => "2"
+			)				
+		),
+		"my_applications" => array(
+			"Πρόσφατες Αιτήσεις",
+			array(
+				"declined" => "Απορρίφθηκαν",
+				"accepted" => "Εγκρίθηκαν"
+			)				
+		)
+	),
+	"announcements" => array(
+		"an1" => "Ανακοινώσεις1",
+		"an2" => "Ανακοινώσεις2"
+	)
+);
+
 function echoSidebar(string $path)
 {
-	$pageNames = array(
-		"profile" => array(
-			"" => "Οι πληροφορίες μου",
-			"account" => "Ο Λογαριασμός μου",
-			"my_applications" => array(
-				"Πρόσφατες Αιτήσεις",
-				array(
-					"declined" => "Απορρίφθηκαν",
-					"accepted" => "Εγκρίθηκαν"
-				)				
-			)
-		),
-		"announcements" => array(
-			"an1" => "Ανακοινώσεις1",
-			"an2" => "Ανακοινώσεις2"
-		)
-	);
-
 	$mainPath = $path;
 	$pathArray = explode("/", $mainPath);
 
@@ -36,19 +42,22 @@ function echoSidebar(string $path)
 	<ul class='list-group ps-0'>
 	";
 
-	foreach ($pageNames[$pathArray[0]] as $subpage => $value) {
+	foreach (pageNames[$pathArray[0]] as $subpage => $value) {
 		$type = gettype($value);
-		if ($type == 'string' || $subpage != $pathArray[1]) {
+		if ($type == 'string') {
 
 			echoSidebarItem("$value", "$pathArray[0]/$subpage", false);
 
 		} else if ($type == 'array') {
-			if (count($pathArray) > 2) {
-				echoSidebarSubmenu($subpage, $value[0], $value[1], $pathArray[2], $pathArray[0]);
+			if ($subpage != $pathArray[1]) {
+				echoSidebarItem($value[0], "$pathArray[0]/$subpage", false);
 			} else {
-				echoSidebarSubmenu($subpage, $value[0], $value[1], "", $pathArray[0]);
+				if (count($pathArray) > 2) {
+					echoSidebarSubmenu($subpage, $value[0], $value[1], $pathArray[2], $pathArray[0]);
+				} else {
+					echoSidebarSubmenu($subpage, $value[0], $value[1], "", $pathArray[0]);
+				}
 			}
-			
 			
 		} else {
 			die("Unexpected error while building sidebar in subpage '$subpage'.");
