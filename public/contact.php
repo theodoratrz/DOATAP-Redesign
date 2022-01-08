c3158128606e53096de98cb83d1c0e2c3c6e0fb4
theodoratrz <theodora.trz@gmail.com>
2022-01-08 02:11:31 +0200

    mail

--- public/contact.php
+++ public/contact.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="/public/css/index.css">
+<link rel="stylesheet" href="/public/css/contact.css">
 
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/template.php" ?>
 
@@ -29,9 +30,15 @@
 <div class="temp-flex">
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/sidebar.php" ?>
 
-    <?php
-        
-    ?>
+    <div class="mail-wrap">
+        <i class="fas fa-envelope"> <a href="mailto:protocol@doatap.gr" style="font-weight:normal;">protocol@doatap.gr</a></i>
+        <span style="font-size:medium"> >Αποστολή Εγγράφων</span>
+        <i class="fas fa-envelope"><a href="mailto:information_dep@doatap.gr" style="font-weight:normal;">information_dep@doatap.gr</a></i>
+        <span style="font-size:medium"> >Πληροφορίες</span>
+        <i class="fas fa-envelope"><a href="mailto:gnisiotita@doatap.gr" style="font-weight:normal;">gnisiotita@doatap.gr</a></i>
+        <span style="font-size:medium"> >Αιτήματα Φορέων για Επαλήθευση Γνησιότητας</span>
+    </div>
+
 
 </div>
 
