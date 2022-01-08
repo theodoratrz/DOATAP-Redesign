3122c08a59d7ef0a70a91f8e6ff5a36eab70bf12
theodoratrz <theodora.trz@gmail.com>
2022-01-08 15:47:52 +0200

    small fixes

--- public/contact.php
+++ public/contact.php
@@ -8,7 +8,7 @@
     <div class="page-container fluid-container">
         <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php" ?>
         <div class="gray-box">
-            <a class="fas fa-arrow-circle-left" onclick="history.back()" style="text-decoration:none; color:#002E69; cursor:pointer;">Επικοινωνία</a>
+            <a class="fas fa-arrow-circle-left" onclick="history.back()" style="text-decoration:none; color:#002E69; cursor:pointer; margin-left:3rem;">Επικοινωνία</a>
         </div>
 
         <div class="page-content-container">
@@ -36,16 +36,14 @@
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3027.5098573999057!2d22.94210651586312!3d40.640690079339684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a839af21cec64b%3A0xdbc815c9573bac04!2zzqXPhs-Fz4DOv8-Fz4HOs861zq_OvyDOnM6xzrrOtc60zr_Ovc6vzrHPgiAtIM6Yz4HOrM66zrfPgiAozpTOuc6_zrnOus63z4TOrs-BzrnOvyk!5e0!3m2!1sel!2sgr!4v1641602256858!5m2!1sel!2sgr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                         <i class="fas fa-phone" style="margin-left:0rem"><a href="tel:2313501372" style="font-size:medium; font-weight:normal; text-decoration:none; color:black;"> 2313-501372</a></i>
                     </div>
-                    
-                    <!--div class="map-wrap">
-                        
-                    </div-->
-                    
-                    <!--div class="map-wrap">
-                        
-                        
-                    </div-->
                 </div>
+                <div class="mail-wrap" style="font-size:medium; width:50%;">
+                    Η δια ζώσης εξυπηρέτηση του κοινού πραγματοποιείται από <b style="font-size:medium">Δευτέρα έως Πέμπτη και ώρες 9:00-12:00.</b>
+                    Προς ανάσχεση της διασποράς του ιού, η δια ζώσης εξυπηρέτηση από υπαλλήλους περιορίζεται στο κοινό που προσέρχεται για να παραλάβει έγγραφα. Το κοινό που προσέρχεται για ενημέρωση εξυπηρετείται από υπαλλήλους τηλεφωνικά από την είσοδο του κτιρίου.
+                </div>
+                    <div class="mail-wrap" style="font-size:medium">
+                    Τηλεφωνική εξυπηρέτηση του κοινού πραγματοποιείται από <b style="font-size:medium">Δευτέρα έως Παρασκευή και ώρες 12:30-14:00</b>
+                        </div>
             </div>
         </div>
     </div>
