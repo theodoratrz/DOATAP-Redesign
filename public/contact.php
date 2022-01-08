e38301d3c6a1eeecf585f9f7907e2fb09d2af10f
theodoratrz <theodora.trz@gmail.com>
2022-01-08 03:13:33 +0200

    map, address, phone

--- public/contact.php
+++ public/contact.php
@@ -27,19 +27,41 @@
 }
 </style>
 
-<div class="temp-flex">
+<div class="page-content-container">
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/sidebar.php" ?>
 
     <div class="mail-wrap">
         <i class="fas fa-envelope"> <a href="mailto:protocol@doatap.gr" style="font-weight:normal;">protocol@doatap.gr</a></i>
-        <span style="font-size:medium"> >Αποστολή Εγγράφων</span>
+        <span style="font-size:medium; margin-bottom:1.5rem;"> >Αποστολή Εγγράφων</span>
         <i class="fas fa-envelope"><a href="mailto:information_dep@doatap.gr" style="font-weight:normal;">information_dep@doatap.gr</a></i>
-        <span style="font-size:medium"> >Πληροφορίες</span>
+        <span style="font-size:medium; margin-bottom:1.5rem;"> >Πληροφορίες</span>
         <i class="fas fa-envelope"><a href="mailto:gnisiotita@doatap.gr" style="font-weight:normal;">gnisiotita@doatap.gr</a></i>
-        <span style="font-size:medium"> >Αιτήματα Φορέων για Επαλήθευση Γνησιότητας</span>
+        <span style="font-size:medium; margin-bottom:1.5rem;"> >Αιτήματα Φορέων για Επαλήθευση Γνησιότητας</span>
+        <div class="mail-wrap">
+            <div class="map-wrap">
+                <span style="font-weight:600;">ΑΘΗΝΑ</span>
+                <span style="font-weight:600;">ΘΕΣΣΑΛΟΝΙΚΗ</span>
+            </div>
+            <div class="map-wrap">
+                <i class="fas fa-map-marker"></i>
+                <span style="font-size:medium; margin-left:0rem;">Αγίου Κωνσταντίνου 54, 104 37</span>
+
+                <i class="fas fa-map-marker" style="margin-left:6rem"></i>
+                <span style="font-size:medium; margin-left:0rem;">Υπουργείο Μακεδονίας Θράκης-Διοικητήριο, 541 23</span>
+            </div>
+            <div class="map-wrap">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3144.6525639092433!2d23.7203603158019!3d37.98523677972163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1bd2f5bd56263%3A0x28c44f8caa298720!2zzpTOn86RzqTOkc6g!5e0!3m2!1sel!2sgr!4v1641600914610!5m2!1sel!2sgr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3027.5098573999057!2d22.94210651586312!3d40.640690079339684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a839af21cec64b%3A0xdbc815c9573bac04!2zzqXPhs-Fz4DOv8-Fz4HOs861zq_OvyDOnM6xzrrOtc60zr_Ovc6vzrHPgiAtIM6Yz4HOrM66zrfPgiAozpTOuc6_zrnOus63z4TOrs-BzrnOvyk!5e0!3m2!1sel!2sgr!4v1641602256858!5m2!1sel!2sgr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+            <div class="map-wrap">
+                <i class="fas fa-phone"></i>
+                <a href="tel:2105281000" style="font-size:medium; margin-left:0rem; text-decoration:none; color:black;">210-5281000</a>
+
+                <i class="fas fa-phone" style="margin-left:6rem"></i>
+                <a href="tel:2313501372" style="font-size:medium; margin-left:0rem; text-decoration:none; color:black;">2313-501372</a>
+            </div>
+        </div>
     </div>
-
-
 </div>
 
 
