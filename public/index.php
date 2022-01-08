02e3a2fe5c0c25f3594cd9ec6f2acf065a13085a
theodoratrz <theodora.trz@gmail.com>
2022-01-08 19:36:27 +0200

    tried many things to write on th image nothing worked on firefox.

--- public/index.php
+++ public/index.php
@@ -4,19 +4,24 @@
 
 <body>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php" ?>
-<div class="background_box">
-<div class="box">
-     <img src="/public/images/im.png" alt="" width="100%">
-     hello
+    
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/public/images/im.png" class="d-block w-100" alt="..." style="opacity:0.5">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
 </div>
-</div>
-<div class="login-container">
-                <h3 style="text-align:center;">Ανακοινώσεις</h3>
-                <hr>
-            
 
-        </div>
+<div class="menu_icons">
+    <h6><i class="fas fa-file-alt">Χρήσιμα Έγγραφα</i></h6>
+    <span>Εδώ μπορείτε να βρείτε χρήσιμα έγρραφα και αιτήσεις που θα χρειαστεί να συμπληρώσετε, ταξινομημένα και σε ηλεκτρονική μορφή</span>
+</div>
 
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php" ?>
 
 </body>
+
