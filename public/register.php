217afa29f2c3b31246c252469d8d16b5e7270a14
theodoratrz <theodora.trz@gmail.com>
2022-01-08 21:57:33 +0200

    register form

--- public/register.php
+++ public/register.php
@@ -13,139 +13,55 @@
         <div class="gray-box">
                 <a class="fas fa-arrow-circle-left" onclick="history.back()" style="text-decoration:none; color:#002E69; cursor:pointer; margin-left:13rem; margin-top:1.7rem;">Εγγραφή</a>
             </div>
-        <div class="login-container-wrapper">
-            <div class="login-container">
-
-                <form class="well form-horizontal" action=" " method="post"  id="contact_form">
-                <fieldset>
-
-                    <!-- Form Name -->
-                    <legend><center><h3>ΔΗΜΙΟΥΡΓΙΑ ΛΟΓΑΡΙΑΣΜΟΥ</h3></center></legend><br>
+            <form class="login-container-wrapper">
+                <div class="login-container">
+                    <h1>Register</h1>
+                    <p>Please fill in this form to create an account.</p>
                     <hr>
-                    <h7 style="text-align:center;">Συμπληρώστε τα στοιχεία σας ώστε να αποκτήσετε πρόσβαση</h7>
-                    <br>
-                    <h7 style="text-align:center;"> σε όλες τις δυνατότητες της πλατφόρμας του ΔΟΑΤΑΠ.</h7>
-                    <br>
-                    <h7 style="text-align:center;"><b>Όλα τα πεδία είναι υποχρεωτικά.</b></h7>
+
+                    <label for="name"><b>ΟΝΟΜΑ</b></label>
+                    <input type="text" placeholder="Όνομα" name="name" id="name" required>
+
+                    <label for="last_name"><b>ΕΠΩΝΥΜΟ</b></label>
+                    <input type="password" placeholder="Επώνυμο" name="last_name" id="last_name" required>
+
+                    <label for="fathers_name"><b>ΠΑΤΡΩΝΥΜΟ</b></label>
+                    <input type="text" placeholder="Πατρώνυμο" name="fathers_name" id="fathers_name" required>
+
+                    <label for="mothers_name"><b>ΜΗΤΡΩΝΥΜΟ</b></label>
+                    <input type="text" placeholder="Μητρώνυμο" name="mothers_name" id="mothers_name" required>
+
+                    <label for="username"><b>ΟΝΟΜΑ ΧΡΗΣΤΗ</b></label>
+                    <input type="text" placeholder="όνομα Χρήστη" name="username" id="username" required>
+
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" placeholder="Email" name="email" id="email" required>
+
+                    <label for="phone"><b>ΤΗΛΕΦΩΝΟ</b></label>
+                    <input type="number" placeholder="Τηλέφωνο" name="phone" id="phone" required>
+
+                    <label for="psw"><b>ΚΩΔΙΚΟΣ</b></label>
+                    <input type="password" placeholder="Κωδικός" name="psw" id="psw" required>
+
+                    <label for="psw-repeat"><b>Repeat Password</b></label>
+                    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+
+                    <label for="id_type"><b>ΕΓΓΡΑΦΟ ΤΑΥΤΟΠΟΙΗΣΗΣ</b></label>
+                    <input type="radio" id="id" name="id_type" value="ID">
+                    <label for="id_type">ΤΑΥΤΟΤΗΤΑ</label><br>
+                    <input type="radio" id="passport" name="id_type" value="PASSPORT">
+                    <label for="passport">ΔΙΑΒΑΤΗΡΙΟ</label><br>
+
+                    <label for="id_num"><b>ΑΡΙΘΜΟΣ ΕΓΓΡΑΦΟΥ</b></label>
+                    <input type="number" placeholder="Αριθμός Εγγράφου" name="id_num" id="id_num" required>
                     <hr>
-                    <!-- Text input-->
 
-                    <div style="display:flex; flex-direction:row;">
-                        <div class="first_col">
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">ΟΝΟΜΑ</label>  
-                                <div class="col-md-4 inputGroupContainer">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input  name="first_name" placeholder="First Name" class="form-control"  type="text">
-                                    </div>
-                                </div>
-                            </div>
+                    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+                    <button type="submit" class="registerbtn">Register</button>
+                    <p>Already have an account? <a href="#">Sign in</a>.</p>
+                </div>
 
-
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" >ΕΠΩΝΥΜΟ</label> 
-                                <div class="col-md-4 inputGroupContainer">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input name="last_name" placeholder="Last Name" class="form-control"  type="text">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group"> 
-                                <label class="col-md-4 control-label">Department / Office</label>
-                                <div class="col-md-4 selectContainer">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
-                                            <select name="department" class="form-control selectpicker">
-                                            <option value="">Select your Department/Office</option>
-                                            <option>Department of Engineering</option>
-                                            <option>Department of Agriculture</option>
-                                            <option >Accounting Office</option>
-                                            <option >Tresurer's Office</option>
-                                            <option >MPDC</option>
-                                            <option >MCTC</option>
-                                            <option >MCR</option>
-                                            <option >Mayor's Office</option>
-                                            <option >Tourism Office</option>
-                                            </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Username</label>  
-                                <div class="col-md-4 inputGroupContainer">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input  name="user_name" placeholder="Username" class="form-control"  type="text">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                
-                    <div class="sec_col">
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" >ΚΩΔΙΚΟΣ</label> 
-                            <div class="col-md-4 inputGroupContainer">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                    <input name="user_password" placeholder="Password" class="form-control"  type="password">
-                                </div>
-                            </div>
-                        </div>
-
-                    
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" >ΕΠΙΒΕΒΑΙΩΣΗ ΚΩΔΙΚΟΥ</label> 
-                            <div class="col-md-4 inputGroupContainer">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                    <input name="confirm_password" placeholder="Confirm Password" class="form-control"  type="password">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">E-Mail</label>  
-                            <div class="col-md-4 inputGroupContainer">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                                    <input name="email" placeholder="E-Mail Address" class="form-control"  type="text">
-                                </div>
-                            </div>
-                        </div>
-
-                    
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">ΤΗΛΕΦΩΝΟ</label>  
-                            <div class="col-md-4 inputGroupContainer">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                                    <input name="contact_no" placeholder="(639)" class="form-control" type="text">
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Select Basic -->
-
-                        <!-- Success message -->
-                        <div class="alert alert-success" role="alert" id="success_message">Success <i class="glyphicon glyphicon-thumbs-up"></i> Success!.</div>
-
-                            <!-- Button -->
-                            <div class="form-group">
-                                <label class="col-md-4 control-label"></label>
-                                <div class="col-md-4"><br>
-                                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button type="submit" class="btn btn-warning" >&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSUBMIT <span class="glyphicon glyphicon-send"></span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset>
-                </form>
-            </div>
-        </div><!-- /.container -->
+            </form> 
     </div>
         
 </body>
