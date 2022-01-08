fc50004ca654d0f123d9797a7fe65ddc8be4ab6a
theodoratrz <theodora.trz@gmail.com>
2022-01-08 23:53:12 +0200

    icons(two links missing)

--- public/index.php
+++ public/index.php
@@ -8,12 +8,26 @@
     
 <div class="container_photo">
   <img src="/public/images/im.png" alt="Snow" style="width:100%; opacity:0.5">
-  <div class="centered">Centered</div>
+  <div class="centered">ΠΙΟ ΠΡΟΣΦΑΤΗ ΑΝΑΚΟΙΝΩΣΗ</div>
 </div> 
 
-<div class="menu_icons">
-    <h6><i class="fas fa-file-alt">Χρήσιμα Έγγραφα</i></h6>
-    <span>Εδώ μπορείτε να βρείτε χρήσιμα έγρραφα και αιτήσεις που θα χρειαστεί να συμπληρώσετε, ταξινομημένα και σε ηλεκτρονική μορφή</span>
+<div class="menu-icons">
+  <div class="icon-text">
+    <h6><a href="applications-forms.php" class="fas fa-file-alt" style="text-decoration:none; color:inherit"> Χρήσιμα Έγγραφα</a></h6>
+    <span style="color:black"> Βρείτε χρήσιμα έγρραφα και αιτήσεις που θα χρειαστεί να συμπληρώσετε, ταξινομημένα και σε ηλεκτρονική μορφή.</span>
+</div>
+<div class="icon-text">
+    <h6><a href="procedure_submission.php" class="fas fa-pen-alt" style="text-decoration:none; color:inherit"> Νέα Αίτηση</a></h6>
+    <span style="color:black">Βρείτε όλα όσα χρειάζονται για να πραγματοποιήσετε μία νέα αίτηση.</span>
+</div>
+<div class="icon-text">
+    <h6><a href="applications-forms.php" class="fas fa-bell" style="text-decoration:none; color:inherit"> Παρακολούθηση Αίτησης</a></h6>
+    <span style="color:black">Παρακολουθήστε την κατάσταση όλων των αιτήσεων σας.</span>
+</div>
+<div class="icon-text">
+    <h6><a href="applications-forms.php" class="fas fa-book" style="text-decoration:none; color:inherit"> Ο ΔΟΑΤΑΠ</a></h6>
+    <span style="color:black">Ενημερωθείτε για όλες τις δράσεις του οργανισμού.</span>
+</div>
 </div>
 
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php" ?>
