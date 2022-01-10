6aff92a1b34077368b6aaba509e754cf119538a4
theodoratrz <theodora.trz@gmail.com>
2022-01-10 19:23:48 +0200

    construction_page not ready yet

--- public/under_construction.php
+++ public/under_construction.php
@@ -1,20 +1,57 @@
-<link rel="stylesheet" href="/public/css/global.css">
+<!-- purple x moss 2020 -->
+<link rel="stylesheet" href="/public/css/under_construction.css">
 <link rel="stylesheet" href="/public/css/index.css">
 
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/template.php" ?>
+<head>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;900&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
+  <script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 28, 2022 15:37:25").getTime();
 
-<!DOCTYPE html>
-<html>
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d ";
+  document.getElementById("demo1").innerHTML = hours + "h ";
+  document.getElementById("demo2").innerHTML = minutes + "m ";
+  document.getElementById("demo3").innerHTML = seconds + "s ";
+
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
+</head>
 <body>
-
-<div style="flex:0 0 100% backroung-color:#000000f0; height:100%; text-align:center;">
-<div class="container_video">
-<img src="https://media.giphy.com/media/9bTjZrytydVRK/giphy.gif" style=" width:50%;height:50%;">
-  <div class="centered_video">
-      <h4>WE ARE COMING SOON</h4>
-  </div>
-</div> 
-
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/template.php" ?>
+  <div class="mainbox">
+    
+  <i class="err fas fa-cog fa-spin"></i>
+  <i class="err2 fas fa-cog fa-spin"></i>
+  <div style="display:flex; flex-direction: row; column-gap:1rem;  justify-content:center; align-items:center; padding-top:25rem; padding-left:10rem;padding-right:10rem;">
+  
+  <p class="rounded-circle" style="background-color:white; border:2px solid black; padding:1rem;" id="demo"></p>
+  <p class="rounded-circle" style="background-color:white; border:2px solid black; padding:1rem;" id="demo1"></p>
+  <p class="rounded-circle" style="background-color:white; border:2px solid black; padding:1rem;" id="demo2"></p>
+  <p class="rounded-circle" style="background-color:white; border:2px solid black; padding:1rem;" id="demo3"></p>
+</div>
+    <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="index.php">home</a> and try from there.</p></div>
 </div>
 </body>
-</html>
