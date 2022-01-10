a23397d94f896bc2b2138b00fad288a6542e65f1
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-01-10 13:10:14 +0200

    Indentation tweaks

--- components/navbar.php
+++ components/navbar.php
@@ -2,45 +2,45 @@
 
 <?php
 $nav_links = array(
-  "Αιτήσεις" => array(
-	  "demo.php",
-	  array("Διαδικασία Υποβολής" => "procedure_submission.php",
-	  "Η Πορεία μίας Αίτησης" => array(
+	"Αιτήσεις" => array(
+		"demo.php",
+		array("Διαδικασία Υποβολής" => "procedure_submission.php",
+		"Η Πορεία μίας Αίτησης" => array(
 		"Αξιολόγηση Αίτησης" => "applications-evaluation.php",
 		"Χρόνοι Διεκπεραίωσης" => "cc.php",
 		"Υπέρβαση Ουράς Προτεραιότητας" => "cc.php"
-	  ),
-	  "Φόρμες Αιτήσεων" => "applications-forms.php",
-	  "Παράβολα" => "paravola.php")
-  ),
-  "Ανακοινώσεις" => array(
+		),
+		"Φόρμες Αιτήσεων" => "applications-forms.php",
+		"Παράβολα" => "paravola.php")
+	),
+	"Ανακοινώσεις" => array(
 	"demo.php",
 	array("Γενικές Πληροφορίες" => "anouncements.php",
 	"Αποφάσεις Δ.Σ" => "aa.php",
 	"Προϋπολογισμοί-Προκηρύξεις" => "aa.php",
 	"Εξετάσεις Οδοντιατρικής" => "aa.php",
 	"Εξετάσεις Ιατρικής" => "aa.php")
-  ),
-  "Ενημέρωση" => array(
+	),
+	"Ενημέρωση" => array(
 	"demo.php",
 	array("Ανώτατα Εκπαιδευτικά Ιδρύματα" => "aa.php",
 	"Εκπαιδευτικά Ιδρύματα Αλλοδαπής" => "aa.php",
 	"Το Ελληνικό Σύστημα Εκπαίδευσης" => "aa.php",
 	"Δι@υγεια" => "aa.php")
-  ),
-  "Οργανισμός" => array(
+	),
+	"Οργανισμός" => array(
 	"demo.php",
 	array("Σύσταση του ΔΟΑΤΑΠ" => "aa.php",
 	"Ετήσιος Απολογισμός ΔΣ" => "aa.php",
 	"Στατιστικά Στοιχεία" => "aa.php")
-  ),
-  "Επικοινωνία" => "contact.php"
+	),
+	"Επικοινωνία" => "contact.php"
 
 );
 
 function echoNavbarMainItem(string $title, string $href)
 {
-  echo '
+	echo '
 	<li class="nav-item">
 		<a class="nav-link nav-pills" aria-current="page" href="' . $href . '">' . $title . '</a>
 	</li>';
@@ -90,7 +90,7 @@ function echoNavbarMenuItem(string $title, string $href)
 
 function echoNavbarContent(array $content)
 {
-  foreach ($content as $title => $value) {
+	foreach ($content as $title => $value) {
 		if (gettype($value) === "array") {
 			$href = $value[0];
 			$subitems = $value[1];
@@ -111,62 +111,62 @@ function echoNavbarContent(array $content)
 ?>
 
 <nav class="primary-navbar navbar-expand-lg bg-primary navbar-dark">
-  <div class="container d-flex">
+	<div class="container d-flex">
 
 	<a class="navbar-brand" href="index.php"> <img src="/public/images/doatap-logo.png" alt="" width="250px"> </a>
 
 	<!-- LANGUAGE -->
-	  <!--a class="search_button"><i class="fas fa-globe-americas"></i></a>
-	  <select class="language_picker" data-width="fit">
+		<!--a class="search_button"><i class="fas fa-globe-americas"></i></a>
+		<select class="language_picker" data-width="fit">
 		<option><span class="flag-icon flag-icon-gr"></span>Greek</option>
 		<option><span class="flag-icon flag-icon-mx"></span>English</option>
-	  </select-->
+		</select-->
 
-	  <!-- login--> 
-	  <?php
+		<!-- login--> 
+		<?php
 
 
-		if(isset(($_SESSION['user'])) && ($_SESSION['user']) == true){
-		  if($_SESSION['role'] == 'admin'){
-			echo '<li class ="nav-item">
-			</li>';
-		  }else{
-			echo '<li class ="nav-item">
-			</li>';
-		  }
-		}else{
-		  echo '
-		  <div class="login-signup-container d-flex justify-content-end">
-			  
-			  <a href="login.php" class="navbar-button nav-fill nav-pills">Σύνδεση</a>
-			  <span style="color:white">|</span>
-			  <a href="register.php" class="navbar-button nav-fill nav-pills">Εγγραφή</a>
+		if (isset(($_SESSION['user'])) && ($_SESSION['user']) == true) {
+			if ($_SESSION['role'] == 'admin') {
+				echo '<li class ="nav-item">
+				</li>';
+			} else {
+				echo '<li class ="nav-item">
+				</li>';
+			}
+		} else {
+			echo '
+			<div class="login-signup-container d-flex justify-content-end">
+				
+				<a href="login.php" class="navbar-button nav-fill nav-pills">Σύνδεση</a>
+				<span style="color:white">|</span>
+				<a href="register.php" class="navbar-button nav-fill nav-pills">Εγγραφή</a>
 			 
-		  </div>';
+			</div>';
 		}
 
 	?>
 	
-  </div>
+	</div>
 </nav>
 
 <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
-  <div class="container-fluid" style="flex-wrap: wrap; justify-content: center;">
+	<div class="container-fluid" style="flex-wrap: wrap; justify-content: center;">
 	<ul class="nav">
 		
-	  <?php
+		<?php
 		echoNavbarContent($nav_links);
-	  ?>
+		?>
 
 	</ul>
-  </div>
+	</div>
 </nav>
 
 <script>
-  document.addEventListener('click', function(e) {
+	document.addEventListener('click', function(e) {
 	// Hamburger menu
-	if (e.target.classList.contains('hamburger-toggle')) {
-	  e.target.children[0].classList.toggle('active');
-	}
-  })
+		if (e.target.classList.contains('hamburger-toggle')) {
+			e.target.children[0].classList.toggle('active');
+		}
+	})
 </script>
