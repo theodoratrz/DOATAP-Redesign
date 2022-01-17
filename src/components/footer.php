8d672d2cd5c0a6c45a1fdf6a3dd5edcd924ff85c
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-01-17 22:04:05 +0200

    footer alignment fixes

--- src/components/footer.php
+++ src/components/footer.php
@@ -9,20 +9,17 @@
                 <li class="d2">Δευτέρα έως Πέμπτη και ώρες 9:00-12:00</li>
                 <li class="fas fa-phone"><a href="tel:2105281000" style="font-size:medium; font-weight:normal; text-decoration:none; color:white;"> 210-5281000</a></li>
                 <li class="d2">Δευτέρα έως Παρασκευή και ώρες 12:30-14:00</li>
-            </ul>
-        
+            </ul>        
             <ul class="d1 contact-group">
                 <li>Δ.Ο.Α.Τ.Α.Π Θεσσαλονίκη</li>
-                <li class="fas fa-map-marker"><span style="font-size:medium; font-weight:normal;"> Υπουργείο Μακεδονίας Θράκης-Διοικητήριο, 541 23</span></li>
+                <li><i class="fas fa-map-marker"><span style="font-size:medium; font-weight:normal;"> Υπουργείο Μακεδονίας Θράκης-Διοικητήριο, 541 23</span></i></li>
                 <li class="d2">Δευτέρα έως Πέμπτη και ώρες 9:00-12:00</li>
                 <li class="fas fa-phone" style="margin-left:0rem"><a href="tel:2313501372" style="font-size:medium; font-weight:normal; text-decoration:none; color:white;"> 2313-501372</a></li>
                 <li class="d2">Δευτέρα έως Παρασκευή και ώρες 12:30-14:00</li>
             </ul>
             <ul class="d1 contact-group">
                 <li class="fas fa-envelope"> <a href="mailto:protocol@doatap.gr" style="font-weight:normal;color:white;">protocol@doatap.gr</a></li>
-                <br>
                 <li class="fas fa-envelope"> <a href="mailto:information_dep@doatap.gr" style="font-weight:normal; color:white;">information_dep@doatap.gr</a></li>
-                <br>
                 <li class="fas fa-envelope"> <a href="mailto:gnisiotita@doatap.gr" style="font-weight:normal;color:white;">gnisiotita@doatap.gr</a></li>
             </ul>
         </div>
