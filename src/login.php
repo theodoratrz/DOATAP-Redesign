3590250d4902bb46712531e31c59312e2719353e
clicknick64 <nick.paxa@gmail.com>
2022-01-18 23:31:15 +0200

    little fixes

--- src/login.php
+++ src/login.php
@@ -13,7 +13,7 @@
             <a href="index.php" class="fas fa-arrow-circle-left" style="text-decoration:none; color:#002E69; cursor:pointer; margin-left:13rem; margin-top:1.7rem;">Σύνδεση</a>
         </div>
         <div class="login-container-wrapper">
-            <form action="/api/login.php" method="POST" class="login-container">
+            <div class="login-container">
                 <h3 style="text-align:center;">ΣΥΝΔΕΣΗ</h3>
                 <hr>
                 <h7 style="text-align:center;">Συμπληρώστε το όνομα χρήστη και τον κωδικό πρόσβασης</h7>
@@ -22,29 +22,29 @@
                 <h7 style="text-align:center;">Εναλλακτικά μπορείτε να συνδεθείτε μέσω <a href="https://www1.gsis.gr/oauth2server/login.jsp"">TaxisNet</a></h7>
                 <hr>
                 <div id=" error-message">
-        </div>
-        <div class="mb-3">
-            <i class="fas fa-user-circle"></i>
-            <label for="username-input" class="form-label">Όνομα Χρήστη</label>
-            <input type="text" class="form-control" id="username-input" required>
-        </div>
-        <div class="mb-3">
-            <i class="fas fa-key"></i>
+            </div>
+            <div class="mb-3">
+                <i class="fas fa-user-circle"></i>
+                <label for="username-input" class="form-label">Όνομα Χρήστη</label>
+                <input type="text" class="form-control" id="username-input" required>
+            </div>
+            <div class="mb-3">
+                <i class="fas fa-key"></i>
 
-            <label for="password-input" class="form-label">Κωδικός</label>
-            <input type="password" class="form-control" id="password-input" required>
+                <label for="password-input" class="form-label">Κωδικός</label>
+                <input type="password" class="form-control" id="password-input" required>
 
+            </div>
+            <div class="mb-3">
+                <label>
+                    <input type="checkbox" class="form-check-input" name="remember">Remember me
+                </label>
+            </div>
+            <button id="submit-button" class="btn btn-primary">Σύνδεση</button>
+            <div class="dropdown-divider"></div>
+            <a class="forgot-item" style="color:blue; text-decoration:double; font-size:medium; text-align:right" href="#">Ξεχάσατε τον κωδικό σας;</button>
+                <a class="create-item" style="color:blue; text-decoration:underline; font-size:x-large; text-align:center" href="register.php">Δημιουργία Λογαριασμού</a>
         </div>
-        <div class="mb-3">
-            <label>
-                <input type="checkbox" class="form-check-input" name="remember">Remember me
-            </label>
-        </div>
-        <button id="submit-button" class="btn btn-primary">Σύνδεση</button>
-        <div class="dropdown-divider"></div>
-        <a class="forgot-item" style="color:blue; text-decoration:double; font-size:medium; text-align:right" href="#">Ξεχάσατε τον κωδικό σας;</button>
-            <a class="create-item" style="color:blue; text-decoration:underline; font-size:x-large; text-align:center" href="register.php">Δημιουργία Λογαριασμού</a>
-            </form>
     </div>
 
     </div>
