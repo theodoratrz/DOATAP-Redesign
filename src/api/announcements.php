31c508439b5669eb62badd238797e0b8192c61a0
clicknick64 <nick.paxa@gmail.com>
2022-01-23 18:01:46 +0200

    Announcements order by date

--- src/api/announcements.php
+++ src/api/announcements.php
@@ -6,10 +6,10 @@ function getAnnouncements($type = 'all')
 {
     global $conn;
     if ($type === 'all'){
-        $sql = 'SELECT * FROM announcements';
+        $sql = 'SELECT * FROM announcements ORDER BY time_uploaded DESC';
     }
     else{
-        $sql = "SELECT * FROM announcements WHERE `type`='$type'";
+        $sql = "SELECT * FROM announcements WHERE `type`='$type' ORDER BY time_uploaded DESC";
     }
     $result = $conn->query($sql);
     $rows = $result->fetch_all(MYSQLI_ASSOC);
