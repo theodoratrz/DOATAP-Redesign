062604f682e024c6d364a109e0b5d846ecb5433f
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-01-23 21:02:52 +0200

    Fix in document paths

--- src/api/documents.php
+++ src/api/documents.php
@@ -3,23 +3,24 @@
 function getApplicationDocuments($applicationID)
 {
     global $conn;
-    $sql = "SELECT * FROM documents WHERE app_id = $applicationID AND type = 'id'";
-    $id = $conn->query($sql)->fetch_all(MYSQLI_ASSOC)[0];
-    $sql = "SELECT * FROM documents WHERE app_id = $applicationID AND type = 'par'";
+    $sql = "SELECT * FROM documents WHERE `app_id` = $applicationID AND `type` = 'id'";
+    $id = $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+    $sql = "SELECT * FROM documents WHERE `app_id` = $applicationID AND `type` = 'par'";
     $par = $conn->query($sql)->fetch_all(MYSQLI_ASSOC)[0];
-    $sql = "SELECT * FROM documents WHERE app_id = $applicationID AND type = 'app'";
+    $sql = "SELECT * FROM documents WHERE `app_id` = $applicationID AND `type` = 'app'";
     $app = $conn->query($sql)->fetch_all(MYSQLI_ASSOC)[0];
+
     return array(
         "id" => array(
-            $id['file_location'],
+            str_replace("/var/www/html", "", $id['file_location']),
             $id['approved']
         ),
         "title" => array(
-            $app['file_location'],
+            str_replace("/var/www/html", "", $app['file_location']),
             $app['approved']
         ),
         "fee" => array(
-            $par['file_location'],
+            str_replace("/var/www/html", "", $par['file_location']),
             $par['approved']
         ),
     );
